fe0ca3ba6c3248a304fac702139042766757fc14
Tony <un_razum@mail.ru>
2022-10-12 23:17:08 +0300

    fix: fix some files

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,32 +1,54 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement chainMaker object according to task description
- * 
+ *
  */
 const chainMaker = {
-  getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    array: [],
+
+    getLength() {
+        return this.array.length;
+        throw new NotImplementedError("Not implemented");
+        // remove line with error and write your code here
+    },
+    // addLink(/* value */) {
+    addLink(value) {
+        this.array.push(String(value));
+        return this;
+        throw new NotImplementedError("Not implemented");
+        // remove line with error and write your code here
+    },
+    removeLink(position) {
+        if (
+            typeof position != "number" ||
+            position % 1 != 0 ||
+            (position > 0 && position <= this.array.length)
+        ) {
+            this.array.splice(position - 1, 1);
+            return this;
+        }
+        this.array = [];
+        throw new Error("You can't remove incorrect link!");
+        throw new NotImplementedError("Not implemented");
+        // remove line with error and write your code here
+    },
+    reverseChain() {
+        this.array.reverse();
+        return this;
+
+        throw new NotImplementedError("Not implemented");
+        // remove line with error and write your code here
+    },
+    finishChain() {
+        let str = `( ${this.array.join(" )~~( ")} )`;
+        this.array = [];
+        return str;
+        throw new NotImplementedError("Not implemented");
+        // remove line with error and write your code here
+    },
 };
 
 module.exports = {
-  chainMaker
+    chainMaker,
 };
