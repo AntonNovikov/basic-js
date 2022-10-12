4da295362a71778d910c9989564efff9f4360b0c
Tony <un_razum@mail.ru>
2022-10-12 23:08:33 +0300

    fix: some problems fix

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,23 +1,54 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+    // throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+    if (typeof members != "object") {
+        return false;
+    }
+    if (!members) {
+        return false;
+    }
+    let isArray = !Array.isArray(members);
+    if (isArray) {
+        return false;
+    }
+    let arrStr = [];
+    for (const i of members) {
+        if (typeof i === "string") {
+            arrStr.push(i);
+        }
+    }
+
+    let arrFirst = [];
+    for (let i of arrStr) {
+        i = i.trim();
+        i = i.substring(0, 1);
+        i = i.toUpperCase();
+        arrFirst.push(i);
+    }
+
+    // arrFirst = [...new Set(arrFirst)];
+
+    arrFirst.sort((a, b) => a.localeCompare(b));
+    arrFirst = arrFirst.join("");
+
+    return arrFirst;
 }
 
 module.exports = {
-  createDreamTeam
+    createDreamTeam,
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,23 +1,60 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function transform(arr) {
+    if (!Array.isArray(arr)) {
+        throw new Error("'arr' parameter must be an instance of the Array!");
+    }
+    if (arr == []) return arr;
+    let newArr = [];
+    for (i = 0; i < arr.length; i++) {
+        if (
+            arr[i] != "--discard-next" &&
+            arr[i] != "--discard-prev" &&
+            arr[i] != "--double-next" &&
+            arr[i] != "--double-prev"
+        ) {
+            newArr.push(arr[i]);
+            continue;
+        }
+        switch (arr[i]) {
+            case "--discard-next":
+                if (i == arr.length - 1) break;
+                i++;
+                if (arr[i + 1] == "--double-prev" || arr[i + 1] == "--discard-prev")
+                    i++;
+                break;
+            case "--double-next":
+                if (i == arr.length - 1) break;
+                newArr.push(arr[i + 1]);
+                newArr.push(arr[i + 1]);
+                i++;
+                break;
+            case "--discard-prev":
+                if (i == 0) break;
+                newArr.pop();
+                break;
+            case "--double-prev":
+                if (i == 0) break;
+                newArr.push(arr[i - 1]);
+        }
+    }
+    return newArr;
 }
 
 module.exports = {
-  transform
+    transform,
 };
