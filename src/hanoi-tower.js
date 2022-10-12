87f7452329850b920139bd648500942dffbb8e6a
Tony <un_razum@mail.ru>
2022-10-12 21:23:06 +0300

    fix: tast hanoi-tower

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -14,11 +14,13 @@ const { NotImplementedError } = require('../extensions/index.js');
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+// function calculateHanoi(/* disksNumber, turnsSpeed */) {
+function calculateHanoi(disksNumber, turnsSpeed) {
+    // throw new NotImplementedError('Not implemented');
+    return 2 ** disksNumber / turnsSpeed * 1000;
+    // remove line with error and write your code here
 }
 
 module.exports = {
-  calculateHanoi
+    calculateHanoi
 };
